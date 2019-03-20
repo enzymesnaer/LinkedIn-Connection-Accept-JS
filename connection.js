aa3293705817a463b27432d59aa58852cfae48aa
Vipin Singh <vs181715@gmail.com>
2019-03-20 12:55:05 +0530

    Create connection.js

---
+++ connection.js
@@ -0,0 +1,11 @@
+// Copy This Code And Paste It on The Console of LinkedIn Page.
+
+var connection = document.getElementByClassName("button-secondary-medium");
+
+//button-secondary-medium is a class name of Accept button
+
+for(i=0;i<connection.length;i++)
+{
+  connection[i].click();
+}
+
