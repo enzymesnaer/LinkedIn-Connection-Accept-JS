26ea00d507a9bafe6944d8ab73a3ee178254087f
スニハル  コリ <snehar16395@gmail.com>
2019-08-06 20:32:02 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,6 +1,6 @@
 // Copy This Code And Paste It on The Console of LinkedIn Page.
 
-var connection = document.getElementByClassName("artdeco-button--secondary");
+var connection = document.getElementsByClassName("artdeco-button--secondary");
 
 //artdeco-button--secondary is a class name of Accept button
 
